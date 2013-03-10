467b894ef9b42236652b01d636795ccfce7b3445
Emanuele DelBono <emanuele@codiceplastico.com>
2013-03-10 07:36:15 +0100

    routing

--- HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
+++ HelloWebApi/HelloWebApi/App_Start/WebApiConfig.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Net;
+using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Web.Routing;
 
 namespace HelloWebApi
 {
@@ -9,6 +10,22 @@ namespace HelloWebApi
     {
         public static void Register(HttpConfiguration config)
         {
+            config.Routes.MapHttpRoute(
+              name: "PostsCustomAction",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional }
+          );
+
+            config.Routes.MapHttpRoute(
+                name: "PostByDate",
+                routeTemplate: "api/{controller}/{year}/{month}/{day}",
+                defaults: new { month = RouteParameter.Optional, day = RouteParameter.Optional },
+                constraints: new { month = @"\d{0,2}", day = @"\d{0,2}"}
+            );
+
+          
+
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ HelloWebApi/HelloWebApi/Controllers/PostsController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace HelloWebApi.Controllers
+{
+    public class PostsController : ApiController
+    {
+        public IQueryable<Post> Get(int year, int month = 0, int day = 0)
+        {
+            Post p = new Post {Title = "First post:"  + year + month + day};
+            return new List<Post>(){p}.AsQueryable();
+
+            // other code omitted ...
+        }
+
+        [HttpGet]
+        public string Category(int id)
+        {
+            return "Category" + id;
+        }
+    }
+
+    public class Post
+    {
+        public string Title { get; set; }
+    }
+}
