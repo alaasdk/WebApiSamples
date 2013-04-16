1d2dab8dff68b615372fa45c2c5f8a4c225e0930
Emanuele DelBono <emanuele@codiceplastico.com>
2013-04-16 16:44:35 +0200

    hosting

---
+++ HelloWebApi/WebAPISelfHostExample/PostsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace WebAPISelfHostExample
+{
+    public class PostsController : ApiController
+    {
+        public string[] Get()
+        {
+            return new[]{"Wow", "this", "is", "a", "real", "web", "application"};
+        } 
+
+    }
+}
---
+++ HelloWebApi/WebAPISelfHostExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+using HelloWebApi.Controllers;
+
+namespace WebAPISelfHostExample
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    //ValuesController controller = new ValuesController();
+        //    var config = new HttpSelfHostConfiguration("http://localhost:3000");
+        //    config.Routes.MapHttpRoute("default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+        //    var server = new HttpSelfHostServer(config);
+        //    server.OpenAsync().Wait();
+        //    Console.ReadLine();
+        //}
+
+        public static void Main()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+        
+            HttpServer server = new HttpServer(config);
+            HttpClient client = new HttpClient(server);
+            HttpResponseMessage response = client.GetAsync("http://localhost/api/posts").Result;
+            String content = response.Content.ReadAsStringAsync().Result;
+            
+        }
+    }
+}
